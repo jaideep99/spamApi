f7781cc840e6e94d349a8dca820fd1d449cde037
jaideep <jaideep.gedi.17cse@bml.edu.in>
2020-09-25 15:06:23 +0530

    initcommit

--- loaddata.py
+++ loaddata.py
@@ -1,24 +1,24 @@
 import numpy as np
 import pandas as pd
-import nltk
+# import nltk
 import re
-from nltk.corpus import stopwords
-from nltk import PorterStemmer,word_tokenize
+# from nltk.corpus import stopwords
+# from nltk import PorterStemmer,word_tokenize
 
-stopwords = stopwords.words('english')
-stemmer = PorterStemmer()
+# stopwords = stopwords.words('english')
+# stemmer = PorterStemmer()
 
-total_words = 0
-def ir_trim(messages):
-    for i in range(len(messages)):
-        text = messages[i]
-        tokens = word_tokenize(text)
-        tokens = [x for x in tokens if x not in stopwords and len(x)>2]
-        tokens = [stemmer.stem(x) for x in tokens]
-        tokens = ' '.join(tokens)
-        messages[i] = tokens
+# total_words = 0
+# def ir_trim(messages):
+#     for i in range(len(messages)):
+#         text = messages[i]
+#         tokens = word_tokenize(text)
+#         tokens = [x for x in tokens if x not in stopwords and len(x)>2]
+#         tokens = [stemmer.stem(x) for x in tokens]
+#         tokens = ' '.join(tokens)
+#         messages[i] = tokens
 
-    return messages
+#     return messages
 
 def normalise_data(text):
 
