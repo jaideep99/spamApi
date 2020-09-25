916a8a81b27bca48109be04a6764b1c3549ea1b2
jaideep <jaideep.gedi.17cse@bml.edu.in>
2020-09-25 13:23:16 +0530

    initcommit

---
+++ loaddata.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import nltk
+import re
+
+path = 'C:\\Users\\jaide\\OneDrive\\Documents\\VSCODE\\spamfilter\\emails.csv'
+
+
+def normalise_data(text):
+
+    for i in range(len(text)):
+        message = text[i]
+        message.lower()
+        message = message.replace('&','and')
+        message = message.replace('e - mail','email')
+        message = message.replace('Subject','')
+        message = re.sub('([(] )?\d{3}( [)])? (- )?\d{3} - \d{4}','phonenumber',message)
+        message = re.sub('[a-zA-Z0-9._ ]+ @ \w+ \. [a-zA-z]+','emailaddr',message)
+        message = re.sub('(https? : / / )?(www \. )[a-zA-Z0-9]+ \. ([a-zA-Z0-9]+ \. )?\w{2,3} (/ [a-zA-Z0-9]+ )*',' url ',message)
+        message = re.sub(r'[^\w\s]',' ',message)
+        message = re.sub(r'\_',' ',message)
+        message = re.sub('[\[\]\(\)\{\}]',' ',message)
+        message = message.replace('!','exclamation')
+        message = re.sub('\d+','',message)
+        message = re.sub(' \w ',' ',message)
+        message = re.sub('  +',' ',message)
+        
+        text[i] = message
+
+    return text
+
+def get_normalisedData():
+    dt = pd.read_csv(path)
+    data = dt.to_dict()
+    data['text'] = normalise_data(data['text'])
+
+    data = pd.DataFrame(data)
+    data = data[:2736]
+    return data
+
+
+
+
+    
---
+++ predictor.py
@@ -0,0 +1,31 @@
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from loaddata import normalise_data
+import pandas as pd
+from flask import Flask, jsonify, request
+
+
+vectorizer, model = pickle.load(open('spam_model.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+
+def predict():
+
+    data = request.get_json(force=True)
+
+    x = [data['text']]
+    x = normalise_data(x)
+    counts = vectorizer.transform(x)
+    result = model.predict(counts)
+
+    output = {'results': int(result[0])}
+
+    # return data
+    return jsonify(results=output)
+
+
+if __name__ == '__main__':
+    app.run(port = 5000, debug=True)
